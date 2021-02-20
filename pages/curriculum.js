f2a58f74d46735bf4270b3fbd222486b47054e1c
André Barreto <alab.home@gmail.com>
2021-02-20 17:29:25 -0300

    Readme

--- pages/curriculum.js
+++ pages/curriculum.js
@@ -18,9 +18,11 @@ export default function Home() {
         
       </section>
 
+      <br></br>
+
       <ul className={utilStyles.list}>
           <li className={utilStyles.listItem}>
-              <a href="https://github.com/alabvix" target="_blank">Curriculum in English</a>
+              <a href="https://docs.google.com/document/d/e/2PACX-1vS4LySBgSr9B24Cs6Sp8wDhAWnUK6NXy8xU68TxZPlggoQTjCdgh9PiNsiZGPivOBq6iOr4u92Nb8Ay/pub" target="_blank">Curriculum in English</a>
           </li>
           <li className={utilStyles.listItem}>
               <a href="https://gitlab.com/users/alabvix/groups" target="_blank">Curriculum em Português do Brasil</a>
