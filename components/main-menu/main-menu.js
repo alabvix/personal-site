7ed625f4b03873a1b2e0644fa8861601ba035ce5
André Barreto <alab.home@gmail.com>
2021-02-20 19:25:24 -0300

    center menu

--- components/main-menu/main-menu.js
+++ components/main-menu/main-menu.js
@@ -2,19 +2,21 @@ import Link from 'next/link'
 
 export default function MainMenu(){
     return (
-        <ul class="mainMenu">
-            <li>
-                <Link href="/"><a>Home</a></Link>
-            </li>
-            <li>
-                <Link href="curriculum">Curriculum Vitae</Link>
-            </li>
-            <li>
-                <Link href="personal"><a>Personal Projects</a></Link>
-            </li>
-            <li>
-                <a href="mailto:alab.home@gmail.com">Contact me</a>
-            </li>
-        </ul>
+        <center>
+            <ul class="mainMenu">
+                <li>
+                    <Link href="/"><a>Home</a></Link>
+                </li>
+                <li>
+                    <Link href="curriculum">Curriculum Vitae</Link>
+                </li>
+                <li>
+                    <Link href="personal"><a>Personal Projects</a></Link>
+                </li>
+                <li>
+                    <a href="mailto:alab.home@gmail.com">Contact me</a>
+                </li>
+            </ul>
+        </center>
     )
 }
